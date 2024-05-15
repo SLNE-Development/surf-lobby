1cce608bfa3d19d155083454e384bcfccf194c75
twisti <twisti@slne.dev>
2024-05-15 14:16:07 +0200

    feat: ✨ Update Java version in build container
    Updated the Java version used in the build container from 17 to 21 to enhance build performance and compatibility with newer Java features. This change ensures that our build environment aligns with current Java standards, thereby

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("Build and publish") {
-    container("Run gradle build", "eclipse-temurin:17") {
+    container("Run gradle build", "eclipse-temurin:21") {
         env["REPOSITORY_URL"] = "https://packages.slne.dev/maven/p/surf/maven"
 
         kotlinScript { api ->
