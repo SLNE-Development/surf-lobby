2348a5a23f8d33653b21612770a53a894a6ebe99
twisti <twisti@slne.dev>
2024-05-18 15:16:28 +0200

    feat: ✨ prevent InventoryDragEvent & PlayerSwapHandItemsEvent

--- surf-server-selector-bukkit/surf-server-selector-bukkit-lobby/src/main/java/dev/slne/surf/surfserverselector/bukkit/lobby/listener/hotbar/HotbarItemClickEventListener.java
+++ surf-server-selector-bukkit/surf-server-selector-bukkit-lobby/src/main/java/dev/slne/surf/surfserverselector/bukkit/lobby/listener/hotbar/HotbarItemClickEventListener.java
@@ -5,8 +5,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,6 +41,29 @@ public final class HotbarItemClickEventListener implements Listener {
   public void onPlayerDropItem(@NotNull PlayerDropItemEvent event) {
     if (PlayerHotbar.isHotbarItem(event.getPlayer(), event.getItemDrop().getItemStack())) {
       event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
+  public void onPlayerSwapHandItems(@NotNull PlayerSwapHandItemsEvent event) {
+    final Player player = event.getPlayer();
+
+    if (PlayerHotbar.isHotbarItem(player, event.getMainHandItem()) || PlayerHotbar.isHotbarItem(player, event.getOffHandItem())) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
+  public void onInventoryDrag(InventoryDragEvent event) {
+    if (!(event.getWhoClicked() instanceof Player clicker)) {
+      return;
+    }
+
+    for (final ItemStack item : event.getNewItems().values()) {
+      if (PlayerHotbar.isHotbarItem(clicker, item)) {
+        event.setCancelled(true);
+        return;
+      }
     }
   }
 }
