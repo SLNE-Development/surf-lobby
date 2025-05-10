ad2a68dc3f2e17200ca0a3a626408be748ce334f
Ammo <ammo@slne.dev>
2025-05-10 22:56:22 +0200

    fix: remove ChooseInitialServerEventListener from ListenerManager

--- surf-lobby-velocity/src/main/java/dev/slne/surf/lobby/velocity/listener/ListenerManager.java
+++ surf-lobby-velocity/src/main/java/dev/slne/surf/lobby/velocity/listener/ListenerManager.java
@@ -3,7 +3,6 @@ package dev.slne.surf.lobby.velocity.listener;
 import dev.slne.surf.lobby.velocity.VelocityMain;
 import dev.slne.surf.lobby.velocity.listener.player.PlayerQuitEventListener;
 import dev.slne.surf.lobby.velocity.listener.player.PlayerSwitchServerEventListener;
-import dev.slne.surf.lobby.velocity.listener.player.ChooseInitialServerEventListener;
 
 public final class ListenerManager {
 
@@ -13,7 +12,6 @@ public final class ListenerManager {
   }
 
   public void registerListeners() {
-    register(new ChooseInitialServerEventListener());
     register(new PlayerQuitEventListener());
     register(new PlayerSwitchServerEventListener());
   }
