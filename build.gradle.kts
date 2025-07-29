3f90e4d707398ad255f674d6e412dd9a14d78009
Ammo <ammo@slne.dev>
2025-07-29 23:55:45 +0200

    fix: update Gradle configuration and remove deprecated platform types

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,13 @@ buildscript {
         maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
     }
     dependencies {
-        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.4+")
+        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.7+")
     }
 }
 
 allprojects {
     group = "dev.slne.surf"
-    version = "1.21.4-1.3.0-SNAPSHOT"
+    version = "1.21.7-1.4.0-SNAPSHOT"
 
     repositories {
         slnePrivate()
