6ed98edf5a834d39bd42ad4907f406943d309216
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-21 10:15:18 +0100

    feat: extend DamageListener to cancel player damage from other players
    - Added handling for `EntityDamageByEntityEvent` to cancel damage caused by players.

--- src/main/kotlin/dev/slne/surf/lobby/listener/DamageListener.kt
+++ src/main/kotlin/dev/slne/surf/lobby/listener/DamageListener.kt
@@ -6,12 +6,20 @@ import dev.slne.surf.surfapi.bukkit.api.event.cancel
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
 
 object DamageListener : Listener {
     @EventHandler
     fun onDamage(event: EntityDamageEvent) {
         if (event.entity is Player) {
+            event.cancel()
+        }
+    }
+
+    @EventHandler
+    fun onDamageByEntity(event: EntityDamageByEntityEvent) {
+        if (event.damager is Player) {
             event.cancel()
         }
     }
