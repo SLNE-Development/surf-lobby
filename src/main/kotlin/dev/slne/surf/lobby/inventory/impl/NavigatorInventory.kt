52a047a8970fa301d3b377957fadeef4bb5399e6
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-21 01:42:57 +0100

    feat: add Navigator inventory and corresponding item
    - Introduced `NavigatorInventory` with a simple menu setup.
    - Created `NavigatorInventoryItem` to interact with the navigator, including item display and interaction logic.

---
+++ src/main/kotlin/dev/slne/surf/lobby/inventory/impl/NavigatorInventory.kt
@@ -0,0 +1,8 @@
+package dev.slne.surf.lobby.inventory.impl
+
+import dev.slne.surf.surfapi.bukkit.api.inventory.dsl.menu
+import dev.slne.surf.surfapi.core.api.messages.adventure.text
+
+fun navigatorInventory() = menu(text("Navigator")) {
+
+}
