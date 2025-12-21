3dd11fffdaf004e6788745be705216af2ba5a1f7
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-21 11:12:07 +0100

    feat: enhance NavigatorInventory with static panes for server selection
    - Added static panes for survival, event, and lobby servers with corresponding items.
    - Updated menu design with slot-based organization and event cancellation logic.

--- src/main/kotlin/dev/slne/surf/lobby/inventory/impl/NavigatorInventory.kt
+++ src/main/kotlin/dev/slne/surf/lobby/inventory/impl/NavigatorInventory.kt
@@ -1,8 +1,63 @@
 package dev.slne.surf.lobby.inventory.impl
 
+import com.github.stefvanschie.inventoryframework.pane.util.Slot
+import dev.slne.surf.surfapi.bukkit.api.builder.displayName
+import dev.slne.surf.surfapi.bukkit.api.event.cancel
 import dev.slne.surf.surfapi.bukkit.api.inventory.dsl.menu
+import dev.slne.surf.surfapi.bukkit.api.inventory.dsl.staticPane
 import dev.slne.surf.surfapi.core.api.messages.adventure.text
+import org.bukkit.inventory.ItemType
 
-fun navigatorInventory() = menu(text("Navigator")) {
+fun navigatorInventory() = menu(text("<shift:-46><glyph:server_selector>"), 6) {
+    setOnGlobalDrag { it.cancel() }
+    setOnGlobalClick { it.cancel() }
 
+    staticPane(Slot.fromXY(5, 0), 3, 3) {
+        fillWith(eventServerItem)
+    }
+
+    staticPane(Slot.fromXY(1, 0), 3, 3) {
+        fillWith(survivalServerItem)
+    }
+
+    staticPane(Slot.fromXY(0, 4), 2, 3) {
+        fillWith(lobbyOneServerItem)
+    }
+
+    staticPane(Slot.fromXY(3, 4), 2, 3) {
+        fillWith(lobbyTwoServerItem)
+    }
+    staticPane(Slot.fromXY(6, 4), 2, 3) {
+        fillWith(lobbyThreeServerItem)
+    }
+}
+
+private val survivalServerItem = ItemType.PAPER.createItemStack().apply {
+    displayName {
+        primary("Survival Server")
+    }
+}
+
+private val eventServerItem = ItemType.PAPER.createItemStack().apply {
+    displayName {
+        primary("Event Server")
+    }
+}
+
+private val lobbyOneServerItem = ItemType.PAPER.createItemStack().apply {
+    displayName {
+        primary("Lobby 1")
+    }
+}
+
+private val lobbyTwoServerItem = ItemType.PAPER.createItemStack().apply {
+    displayName {
+        primary("Lobby 2")
+    }
+}
+
+private val lobbyThreeServerItem = ItemType.PAPER.createItemStack().apply {
+    displayName {
+        primary("Lobby 3")
+    }
 }
