4754fecd4a262cbf9605e9ef07daee959a985345
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-24 14:36:21 +0100

    fix: update slots for Pushback inventory items

--- src/main/kotlin/dev/slne/surf/lobby/inventory/item/impl/PushbackEnableInventoryItem.kt
+++ src/main/kotlin/dev/slne/surf/lobby/inventory/item/impl/PushbackEnableInventoryItem.kt
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemType
 
 object PushbackEnableInventoryItem : InventoryItem {
-    override val slot = 3
+    override val slot = 0
     override val permission: String = PermissionRegistry.PUSHBACK_ITEM
     override val item = ItemType.ENDER_EYE.createItemStack().apply {
         displayName {
