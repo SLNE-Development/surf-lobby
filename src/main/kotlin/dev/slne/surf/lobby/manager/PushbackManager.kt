1b36f26ad843bf1450c76495f77e8ff3e1d4b661
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-15 15:49:18 +0100

    feat: reduce delay between punch sound effects in PushbackManager

--- src/main/kotlin/dev/slne/surf/lobby/manager/PushbackManager.kt
+++ src/main/kotlin/dev/slne/surf/lobby/manager/PushbackManager.kt
@@ -30,7 +30,7 @@ object PushbackManager {
 
                 player.world.playEffect(player.location, Effect.ENDER_SIGNAL, null)
             }
-        }, 20, 20)
+        }, 0, 10)
     }
 
     fun add(uuid: UUID) {
